ff3dbc559974bfe9f033eb3141414c5e11e576b6
大崎向士 <osaki@Koshi-MacBook-Air.local>
2022-04-24 20:14:42 +0900

    バグの修正

--- src/components/ImgDropZone.tsx
+++ src/components/ImgDropZone.tsx
@@ -2,7 +2,9 @@ import { useDropzone } from "react-dropzone";
 
 export const ImgDropZone = () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-  const files = acceptedFiles.map((file: any) => <li>{file.path}</li>);
+  const files = acceptedFiles.map((file: any) => (
+    <li key="file">{file.path}</li>
+  ));
   return (
     <div className="container bg-red-200 mt-5 p-4">
       <div {...getRootProps({ className: "dropzone" })}>
